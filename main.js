afff51df4348aea4f1f9a72f95b54750f00a3125
Joseph Oritseweyinmi <josephoritseweyinmi5@gmail.com>
2023-11-03 07:26:27 +0100

    added swiperJs, hero images and updated the styles

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 import './style.css';
+import Swiper from 'swiper';
+import 'swiper/css';
+
+const swiper = new Swiper('.swiper', {
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  grabCursor: true,
+  loop: true,
+});
