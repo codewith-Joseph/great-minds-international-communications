bf24c646c2ebde04a4217a68cc4f6c74dea7b75d
Joseph Oritseweyinmi <josephoritseweyinmi5@gmail.com>
2023-11-04 17:31:18 +0100

    Added the contact form, the FAQ, changed the body background and added the Poppins font

--- main.js
+++ main.js
@@ -24,3 +24,11 @@ menuToggleBtn.addEventListener('click', () => {
   closeIcon.classList.toggle('hidden');
   navigationMenu.classList.toggle('hidden');
 });
+
+const accordion = document.getElementsByClassName('contentBx');
+
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener('click', () => {
+    accordion[i].classList.toggle('active');
+  });
+}
